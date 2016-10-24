475ce233d6d787a4ce4e78b5141c8b90d112e089
KisliakVladimir <Kisliak.Vladimir@tut.by>
2016-10-24 14:28:45 +0300

    добавил в библиотеку к методу Ньютона метод Pow и метод сравнения. Сделал консольку

---
+++ Task2/ConsoleTask2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2Library;
+
+
+namespace ConsoleTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите чило A возводимое, которое нужно возвести в степень:");
+            double A;
+            if (!Double.TryParse(Console.ReadLine(), out A))
+            {
+                do
+                {
+                    Console.WriteLine("Введите корректное значение doble для A");
+                }
+                while (!Double.TryParse(Console.ReadLine(), out A));
+             
+            }
+            double N;
+            Console.WriteLine("Введите чило n (степень):");
+            if (!Double.TryParse(Console.ReadLine(), out N))
+            {
+                do
+                {
+                    Console.WriteLine("Введите корректное значение doble для n");
+                }
+                while (!Double.TryParse(Console.ReadLine(), out N));
+
+            }
+            NewTon k = new NewTon(A, N);
+            Console.WriteLine("А в степени n по методу Ньютона с погрешностью 0,0001 равно: " + k.SqrtN());
+            Console.WriteLine("A в степени n по методу Pow библиотеки NET равно: " + k.Pow());
+            Console.WriteLine(k.CompairNewtonandPow(k.SqrtN(), k.Pow()));
+            Console.ReadLine();
+
+        }
+    }
+}
--- Task2/Task2Library/NewTon.cs
+++ Task2/Task2Library/NewTon.cs
@@ -8,18 +8,50 @@ namespace Task2Library
 {
     public class NewTon
     {
-        static double SqrtN(double n, double A, double eps = 0.0001)
-        {
-            var x0 = A / n;
-            var x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
 
-            while (Math.Abs(x1 - x0) > eps)
+        public double A { get; set; }
+        public double N { get; set; }
+        public double Eps { get; set; }
+
+
+        public NewTon (double a, double n)
+        {
+            this.A = a;
+            this.N = n;
+            this.Eps = 0.0001;
+        }
+        public double SqrtN()
+        {
+            var x0 = A / N;
+            var x1 = (1 / N) * ((N - 1) * x0 + A / Math.Pow(x0, (int)N - 1));
+
+            while (Math.Abs(x1 - x0) > Eps)
             {
                 x0 = x1;
-                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+                x1 = (1 / N) * ((N - 1) * x0 + A / Math.Pow(x0, (int)N - 1));
             }
 
             return x1;
+        }
+
+        public double Pow()
+        {
+            return Math.Pow(A, N);
+        }
+
+
+        public string CompairNewtonandPow(double newton, double pow)
+        {
+            
+            if (newton.CompareTo(pow)>0)
+            {
+                return "newton > pow";
+            }
+            if (newton.CompareTo(pow) < 0)
+            {
+                return "newton < pow";
+            }
+            return "равны";
         }
     }
 }
